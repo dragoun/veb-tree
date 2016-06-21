da0ff06783e73037df8639f1dc93cdbbf09d3d97
Dominik Dragoun <dragoun@lithium.cz>
2016-06-21 17:11:09 +0200

    adds vEB header file with functions declarations and doxygen comments

---
+++ veb.h
@@ -0,0 +1,175 @@
+/*******************************************************************************
+* Copyright (C) 2016 Dominik Dragoun                                           *
+*                                                                              *
+* This file is part of my Van Emde Boas tree data structure implementation in  *
+* C/C++. It is released under MIT License, which should be distributed with    *
+* this file. It is also avaible at <https://opensource.org/licenses/MIT>.      *
+*******************************************************************************/
+
+/***************************************************************************//**
+ * @file veb.h
+ *
+ * @brief      File containing declarations of a class implementing Van Emde
+ *             Boas tree data structure.
+ * @author     Dominik Dragoun (dominik@dragoun.com)
+ * @date       May, 2016
+ * @copyright  Copyright (C) 2016 Dominik Dragoun.
+ * @license    This project is released undes the MIT License.
+ ******************************************************************************/
+
+#ifndef __VEB_H_458976543568798867538649758687654752463747856374562543646__
+#define __VEB_H_458976543568798867538649758687654752463747856374562543646__
+
+#include <cmath>
+
+/***************************************************************************//**
+ * @brief      Struct containing the Van Emde Boas tree.
+ *
+ * @details    It is a data structure which implements an associative array with
+ *             m-bit integer keys. It supports operations of ordered associative
+ *             array (Insert, Delete and Find) and two more order opperations -
+ *             Find Next (Succ) and Find Previous (Pred). All these operations
+ *             are performed in O(log m) time, or equivalently in O(log log M)
+ *             time, where M = 2^m is the maximum number of elements that can be
+ *             stored in the tree. It also supports the operation Minimum (Min)
+ *             and Maximum (Max), wich returns the minimum and maximum element
+ *             stored in the tree respectively. These both run in O(1) time,
+ *             since the minimum and maximum element are stored as attributes in
+ *             each tree.
+ ******************************************************************************/
+struct TvEB
+{
+  int u;
+  int min;
+  int max;
+  TvEB * summary;
+  TvEB ** cluster;
+};
+
+
+/***************************************************************************//**
+ * @brief      Returns the element's index in the cluster.
+ *
+ * @param[in]  x     The value of the element.
+ * @param[in]  u     The size of the universe.
+ *
+ * @return     The element's index in the cluster.
+ ******************************************************************************/
+int low ( int x, int u )
+{
+  return x % ( int ) sqrt ( u );
+}
+
+/***************************************************************************//**
+ * @brief      Returns the index of the element's cluster.
+ *
+ * @param[in]  x     The value of the element.
+ * @param[in]  u     The size of the universe.
+ *
+ * @return     The index of the element's cluster.
+ ******************************************************************************/
+int high ( int x, int u )
+{
+  return x / ( int ) sqrt ( u );
+}
+
+/***************************************************************************//**
+ * @brief      Finds the lowest value stored in the given tree.
+ *
+ * @param[in]  tree   The pointer to the van Emde Boas tree.
+ * @param[out] res    The lowest element.
+ *
+ * @retval     true   Successfully found the minimum.
+ * @retval     false  Failed to found the minimum.
+ ******************************************************************************/
+bool vEB_min ( TvEB * tree, int & res )
+{
+  if ( tree )
+  {
+    res = tree->min;
+    return true;
+  }
+  return false;
+}
+
+/***************************************************************************//**
+ * @brief      Finds the highest value stored in the given tree.
+ *
+ * @param[in]  tree   The pointer to the van Emde Boas tree.
+ * @param[out] res    The highest element.
+ *
+ * @retval     true   Successfully found the maximum.
+ * @retval     false  Failed to found the maximum.
+ ******************************************************************************/
+bool vEB_max ( TvEB * tree, int & res )
+{
+  if ( tree )
+  {
+    res = tree->max;
+    return true;
+  }
+  return false;
+}
+
+/***************************************************************************//**
+ * @brief      Inserts the given value into the given vEB tree.
+ *
+ * @param[in]  tree   The pointer to the van Emde Boas tree to insert the value.
+ * @param[in]  val    The value of the element to insert.
+ *
+ * @retval     true   Successfully inserted the value.
+ * @retval     false  Failed to insert the value.
+ ******************************************************************************/
+bool vEB_insert ( TvEB * tree, int val );
+
+/***************************************************************************//**
+ * @brief      Removes the given value from the given vEB tree.
+ *
+ * @param[in]  tree   The pointer to the van Emde Boas tree.
+ * @param[in]  val    The value of the element to remove.
+ * @param[out] res    The removed element.
+ *
+ * @retval     true   Successfully removed the value.
+ * @retval     false  Failed to remove the value.
+ ******************************************************************************/
+bool vEB_delete ( TvEB * tree, int val, int & res );
+
+/***************************************************************************//**
+ * @brief      Finds if the given value is in the given vEB tree.
+ *
+ * @param[in]  tree   The pointer to the van Emde Boas tree.
+ * @param[in]  val    The value of the element to find.
+ * @param[out] res    The found element.
+ *
+ * @retval     true   Successfully found the element.
+ * @retval     false  Failed to found the element.
+ ******************************************************************************/
+bool vEB_find ( TvEB * tree, int val, int & res );
+
+/***************************************************************************//**
+ * @brief      Finds the smallest value at least the given value in the given
+ *             tree.
+ *
+ * @param[in]  tree   The pointer to the van Emde Boas tree.
+ * @param[in]  val    The lower bound for the value of the sought element
+ * @param[out] res    The found element.
+ *
+ * @retval     true   Successfully found the successor.
+ * @retval     false  Failed to found the successor.
+ ******************************************************************************/
+bool vEB_succ ( TvEB * tree, int val, int & res );
+
+/***************************************************************************//**
+ * @brief      Finds the largest value at most the given value in the given
+ *             tree.
+ *
+ * @param[in]  tree   The pointer to the van Emde Boas tree.
+ * @param[in]  val    The upper bound for the value of the sought element
+ * @param[out] res    The found element.
+ *
+ * @retval     true   Successfully found the predecessor.
+ * @retval     false  Failed to found the predecessor.
+ ******************************************************************************/
+bool vEB_pred ( TvEB * tree, int val, int & res );
+
+#endif /* __VEB_H_458976543568798867538649758687654752463747856374562543646__ */
